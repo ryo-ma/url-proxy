b64c508a611a64dbdb0956ec13be48c5ee9696d2
ryo-ma <saka_ro@yahoo.co.jp>
2024-03-19 23:11:47 +0900

    Add logic

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-   return "<p>Hello, World!</p>"
+
+@app.route("/", methods=["GET"])
+def proxy():
+    url = request.args.get('url', '')
+    encoding = request.args.get('encoding', 'utf-8')
+    if url == '':
+        return "parameter `url` is required", 400
+    response = requests.get(url)
+    response.encoding = encoding
+    return response.text
